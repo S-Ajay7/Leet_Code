999b6a0322d720bdda6dfe404ecb8e347300a6ad
S-Ajay7 <s.ajay15082002@gmail.com>
2024-09-17 10:44:20 +0530

    Rotated Binary Search Finished !

---
+++ 33. Search in Rotated Sorted Array/Rotated_Binary_Search.java
@@ -0,0 +1,80 @@
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+/** There is an integer array nums sorted in ascending order (with distinct values).
+
+ Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+ Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+ You must write an algorithm with O(log n) runtime complexity.
+
+
+
+ Example 1:
+
+ Input: nums = [4,5,6,7,0,1,2], target = 0
+ Output: 4
+ Example 2:
+
+ Input: nums = [4,5,6,7,0,1,2], target = 3
+ Output: -1
+ Example 3:
+
+ Input: nums = [1], target = 0
+ Output: -1 */
+
+ public class Rotated_Binary_Search {
+    static int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if (pivot == -1) {
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    static int binarySearch(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    static int findPivot(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            } else if (nums[mid] > nums[start]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int res = search(nums, target);
+        System.out.println(res);
+    }
+}
